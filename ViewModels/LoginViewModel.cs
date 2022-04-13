ef47910ba6ca74eb188d748140d01c00a50d1591
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-04-13 19:39:52 -0400

    Add files via upload

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment12._2._2.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter user name")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage ="Please enter a valid password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
