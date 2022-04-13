0f886fd0d07be0b52f5152ff1d129f2928bd6e39
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2022-04-13 19:38:51 -0400

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Assignment12._2._2.ViewModels;
+using Assignment12._2._2.Models;
+
+namespace Assignment12._2._2.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SignInManager<User> _signInManager;
+        private UserManager<User> _userManager;
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            if(this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginView)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginView.UserName, loginView.Password, loginView.RememberMe, false);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Failed to login");
+            return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                User newuser = new User()
+                {
+                    FirstName = registerViewModel.FirstName,
+                    LastName = registerViewModel.LastName,
+                    UserName = registerViewModel.UserName,
+                    PhoneNumber = registerViewModel.PhoneNumber.ToString(),
+                    Email = registerViewModel.Email
+                };
+                var result = await _userManager.CreateAsync(newuser, registerViewModel.Password);
+                if(result.Succeeded)
+                {
+                    var addedUser = await _userManager.FindByNameAsync(newuser.UserName);
+                    return RedirectToAction("Login", "Account");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/DrawController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Assignment12._2._2.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Assignment12._2._2.Controllers
+{
+    public class DrawController : Controller
+    {
+        private IDraw _Draw;
+        public DrawController(IDraw draw)
+        {
+            _Draw = draw;
+        }
+        public IActionResult Index()
+        {
+            DrawViewModel drawViewModel = new DrawViewModel();
+            drawViewModel.Draws = _Draw.InitializeDraw();
+            return View(drawViewModel);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            var model = _Draw.GetDrawById(id);
+            if(model == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Authorize]
+        public IActionResult Create(Draw obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _Draw.AddDraw(obj);
+            }
+            ViewBag.Message = "New draw is created successfully!";
+            return View();
+        }
+        [Authorize]
+        public IActionResult Delete (int id)
+        {
+            _Draw.DeleteDraw(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Update(int id)
+        {
+            Draw draw = _Draw.GetDrawById(id);
+            return View(draw);
+        }
+        [HttpPost]
+        [Authorize]
+        public IActionResult Update(Draw obj, int id)
+        {
+            obj.Id = id;
+            _Draw.UpdateDraw(obj);
+            return RedirectToAction("Index");
+        }
+    }
+}
